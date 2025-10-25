0a3dddaa18f208232684adb8d5599ed552da7380
victor calderon <victoredu.calderon.mo@gmail.com>
2025-10-25 16:38:34 -0600

    Updated music for David...

--- scripts.js
+++ scripts.js
@@ -425,10 +425,8 @@
 
       // Ocultar botón de confirmación
       document.getElementById("confirm").style.display = "none";
-      alert('Bienvenido')
       // --- Cambio de música para invitado especial ---
       if (nombre === 'David Flores') {
-        alert('Hola David')
         const musica = document.getElementById('musica');
         const source = musica.querySelector('source');
         
